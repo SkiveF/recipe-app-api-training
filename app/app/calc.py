c04f59c5831f2feb0aed8e60cb217e49ce176a0c
skivef <skivefameux@gmail.com>
2023-09-05 12:03:04 +0200

    added posgresql at docker-compose file

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""calculator functions
+"""
+
+def add(x,y):
+    """add x and y an return result.
+
+    Args:
+        x (_type_): int
+        y (_type_): int
+    """
+    return x + y
+
+def subtract(x,y):
+    """subtract operation"""
+    return y - x 
